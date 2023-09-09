cb87eab2637431192facf6e756484fe1919204b6
TAVnotDove <slavei.dikovski123@gmail.com>
2023-09-09 23:53:47 +0300

    added basic button counter for testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    console.log("works!")
+
+    let count = 0
+
+    function addOne() {
+        count++
+    }
+</script>
+
+<style>
+    button {
+        padding: 1rem;
+        font-size: 1rem;
+    }
+</style>
+
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<button on:click={addOne}>Clicked {count} Times</button>
