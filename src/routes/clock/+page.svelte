0a5b658726c57feec4614b0addd4eb1ee8ba601b
TAVnotDove <slavei.dikovski123@gmail.com>
2023-09-10 22:35:08 +0300

    added basic clock

--- src/routes/clock/+page.svelte
+++ src/routes/clock/+page.svelte
@@ -1,7 +1,47 @@
-<script></script>
+<script>
+    let hours = stringFormat(new Date().getHours())
+    let minutes = stringFormat(new Date().getMinutes())
+    let clockMode = "24-hour"
+    let twelveM = null
+
+    function stringFormat(number) {
+        return number > 9 ? number : `0${number}`
+    }
+
+    function changeClockMode() {
+        if (clockMode === "12-hour") {
+            clockMode = "24-hour"
+
+            hours = stringFormat(new Date().getHours())
+            minutes = new Date().getMinutes()
+        } else {
+            clockMode = "12-hour"
+            
+            minutes = new Date().getMinutes()
+            
+            if (hours > 12) {
+                twelveM = "PM"
+                hours = stringFormat(hours - 12)
+            } else {
+                twelveM = "AM"
+            }
+        }
+    }
+</script>
 
 <div id="main">
     <h1>Clock</h1>
+
+    <div id="clock-container">
+        {#if clockMode === "12-hour"}
+            <p>{hours}:{minutes}{twelveM}</p>
+        {/if}
+        {#if clockMode === "24-hour"}
+            <p>{hours}:{minutes}</p>
+        {/if}
+        <button on:click={changeClockMode}>{clockMode}</button>
+    </div>
+
     <nav>
         <a href="/stopwatch">Stopwatch</a>
         <a href="/">Home</a>
@@ -9,4 +49,19 @@
     </nav>
 </div>
 
-<style></style>
+<style>
+    #clock-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        background-color: lightgray;
+        padding: 0.5rem;
+        height: 100px;
+        border-radius: 10%;
+    }
+
+    #clock-container p {
+        font-size: 3rem;
+        margin: 0;
+    }
+</style>
